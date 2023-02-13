7850e034bf31977c75d2b666e2075dcb6e5d638f
aicarrera <aicarrera@mondragon.edu>
2023-02-13 12:04:24 +0000

    docker compose

---
+++ APIContextInteraction/graphdb/data/data/settings.js
@@ -0,0 +1,32 @@
+{
+  "import.server" : { },
+  "import.local" : {
+    "interaction_context;;ontology_serviceInteraction.owl" : {
+      "name" : "ontology_serviceInteraction.owl",
+      "status" : "DONE",
+      "message" : "Imported successfully in less than a second.",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/ontology_serviceInteraction.owl",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "469861cd-e174-4e8c-85f2-75e967a0a7c1",
+      "timestamp" : 1676047586341,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      },
+      "requestIdHeadersToForward" : null
+    }
+  },
+  "properties" : {
+    "current.location" : ""
+  }
+}
