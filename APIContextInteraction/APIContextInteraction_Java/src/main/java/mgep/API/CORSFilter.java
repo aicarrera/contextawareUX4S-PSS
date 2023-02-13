e27221eb53a6255b2585f5a953064d33a6a9d046
Angela Isabel Carrera <aicarrera@mondragon.edu>
2023-02-13 21:50:20 +0100

    remove stuff data

--- APIContextInteraction/APIContextInteraction_Java/src/main/java/mgep/API/CORSFilter.java
+++ APIContextInteraction/APIContextInteraction_Java/src/main/java/mgep/API/CORSFilter.java
@@ -27,7 +27,7 @@ public class CORSFilter implements ContainerResponseFilter {
           cres.getHeaders().add("Access-Control-Allow-Origin", "*");
   
       }
-      cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, graphdb_server ");
+      cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, graphdb_server, Access-Control-Allow-Origin");
       cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
       cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
       cres.getHeaders().add("Access-Control-Max-Age", "1209600");
